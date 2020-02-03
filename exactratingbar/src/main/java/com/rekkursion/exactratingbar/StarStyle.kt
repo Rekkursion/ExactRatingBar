747e437a339cf47500314e4cdebc54ba7e51fa2b
Rekkursion <rekkursion233@gmail.com>
2020-02-03 22:01:26 +0800

    Add more shapes: triangle, rhombus, & diamond

--- exactratingbar/src/main/java/com/rekkursion/exactratingbar/StarStyle.kt
+++ exactratingbar/src/main/java/com/rekkursion/exactratingbar/StarStyle.kt
@@ -1,6 +1,7 @@
 package com.rekkursion.exactratingbar
 
 import android.graphics.*
+import androidx.core.math.MathUtils
 import kotlin.math.*
 
 enum class StarStyle(val getShapedPath: (x: Float, y: Float, size: Float) -> Path) {
@@ -38,6 +39,68 @@ enum class StarStyle(val getShapedPath: (x: Float, y: Float, size: Float) -> Pat
         val path = Path()
         // add a square into the path
         path.addRect(x, y, x + size, y + size, Path.Direction.CW)
+        // return the path
+        path
+    }),
+
+    TRIANGLE({ x, y, size ->
+        val r = size / 2F
+        val cX = x + r
+        val cY = y + r
+        // the offset of y-axis
+        val offsetY = 0.25F * r
+
+        // the path object
+        val path = Path()
+        // move to the top point
+        path.moveTo(cX, y + offsetY)
+        // add three lines that comprise a regular triangle
+        for (idx in 0..2) {
+            val angle = ((idx * 120 - 90) * PI / 180).toFloat()
+            path.lineTo(cX + r * cos(angle), cY + r * sin(angle) + offsetY)
+        }
+        path.close()
+        // return the path
+        path
+    }),
+
+    RHOMBUS({ x, y, size ->
+        val r = size / 2F
+        val cX = x + r
+        val cY = y + r
+
+        // the path object
+        val path = Path()
+        // move to the top point
+        path.moveTo(cX, y)
+        // add four lines as a rhombus
+        path.lineTo(x + size, cY)
+        path.lineTo(cX, y + size)
+        path.lineTo(x, cY)
+        path.lineTo(cX, y)
+        path.close()
+        // return the path
+        path
+    }),
+
+    DIAMOND({ x, y, size ->
+        val firstHorizontalLineWidth = size * 0.614F
+        @Suppress("UnnecessaryVariable")
+        val secondHorizontalLineWidth = size
+        val heightBetweenTwoHorizontalLines = size * 0.27333F
+        val totalHeight = size * 0.86F
+
+        // the path object
+        val path = Path()
+        // move to the start point
+        path.moveTo(x + (secondHorizontalLineWidth - firstHorizontalLineWidth) / 2F, y + (size - totalHeight) / 2F)
+        // add lines to construct a shape of diamond
+        path.rLineTo(firstHorizontalLineWidth, 0F)
+        path.rLineTo((secondHorizontalLineWidth - firstHorizontalLineWidth) / 2F, heightBetweenTwoHorizontalLines)
+        path.rLineTo(-(secondHorizontalLineWidth / 2F), totalHeight - heightBetweenTwoHorizontalLines)
+        path.rLineTo(-(secondHorizontalLineWidth / 2F), -(totalHeight - heightBetweenTwoHorizontalLines))
+        path.rLineTo((secondHorizontalLineWidth - firstHorizontalLineWidth) / 2F, -heightBetweenTwoHorizontalLines)
+        path.close()
         // return the path
         path
     });
